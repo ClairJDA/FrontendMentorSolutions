d3caf3ba426cf8dd1b9d0d45473211c423df1ce1
ClairJDA <160573915+ClairJDA@users.noreply.github.com>
2024-09-12 08:59:37 +0700

    Delete article-preview-component-master directory

--- article-preview-component-master/index.js
+++
@@ -1,9 +0,0 @@
-var share = document.getElementById('share-btn');
-share.addEventListener("click", popupmenu);
-
-var popup = document.getElementById('popup-menu');
-popup.addEventListener("click",popupmenu);
-
-function popupmenu(){
-    popup.classList.toggle('hide');
-}
