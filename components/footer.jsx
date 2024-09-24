0eb283271fe0d473e57569a1be914b7385ef4da5
ClairJDA <clair.jda@gmail.com>
2024-09-24 17:38:09 +0700

    update footer key missing

--- components/footer.jsx
+++ components/footer.jsx
@@ -11,18 +11,18 @@ function Footer() {
       <Image src="/images/logo.svg" alt="logo" width={100} height={40} />
       <div className="flex flex-col my-4">
         <p className="mb-2 text-white">Product</p>
-        {productMenu.map(item=> <a className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
+        {productMenu.map(item=> <a key={item.id} className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
       </div>
 
       <div className="flex flex-col my-4">
         <p className="mb-2 text-white">Company</p>
-        {companyMenu.map(item=> <a className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
+        {companyMenu.map(item=> <a key={item.id} className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
         
       </div>
 
       <div className="flex flex-col my-4">
         <p className="mb-2 text-white">Connect</p>
-        {connectMenu.map(item=> <a className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
+        {connectMenu.map(item=> <a key={item.id} className="ubuntu font-extralight hover:cursor-pointer">{item.label}</a>)}
       </div>
     </div>
   );
